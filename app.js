8b2fa8575852e4bdfa3c2758afb65792ba6b0527
ASM Tamzid <atkthegreat9999@gmail.com>
2025-05-28 18:07:59 +0600

    cookie login

--- app.js
+++ app.js
@@ -4,19 +4,32 @@ const cookieParser = require('cookie-parser');
 
 
 app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/set-cookie', (req,res)=>{
-    res.cookie('name', 'This is ASM', {httpOnly: true});
-    res.send('Cookie has been set!');
+app.post('/login', (req, res) => {
+    const { username } = req.body || {};
+    if (!username) {
+        return res.status(400).send('username is required');
+    }
+    res.cookie('name', username, {
+        maxAge: 900000, // 15 minutes
+        httpOnly: true, // Prevents client-side JavaScript from accessing the cookie 
+    });
+    res.send('Cookie is set');
 });
 
-app.get('/check-cookie', (req, res)=>{
-    const myCookie = req.cookies.name;
-    if(myCookie === 'This is ASM') {
-        res.send(`Cookie value: ${myCookie}`);
+app.get('/product', (req, res)=>{
+    const {name} = req.cookies || {};
+    const {username} = req.body || {};
+    if(!name && !username){
+        return res.status(401).send('You are not logged in. Please log in first.');
+    }
+    else if(name == username){
+        return res.status(200).send('Welcome to the product page, asm!');
     }
     else{
-        res.send('not samme cookie');
+        return res.status(403).send('Access denied. You are not allowed to view this page.');
     }
 });
 

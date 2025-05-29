d078d6cd24fcd81a9fcb88cb2d3668f14cb3bc28
ASM Tamzid <atkthegreat9999@gmail.com>
2025-05-29 17:34:20 +0600

    added session Storage

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const sessionStorage = {};
+
 app.post('/login', (req, res) => {
     const { username } = req.body || {};
     if (!username) {
@@ -16,17 +18,15 @@ app.post('/login', (req, res) => {
         maxAge: 900000, // 15 minutes
         httpOnly: true, // Prevents client-side JavaScript from accessing the cookie 
     });
+    sessionStorage[username] = {loggedIn: true};
     res.send('Cookie is set');
 });
 
 app.get('/product', (req, res)=>{
     const {name} = req.cookies || {};
-    const {username} = req.body || {};
-    if(!name && !username){
-        return res.status(401).send('You are not logged in. Please log in first.');
-    }
-    else if(name == username){
-        return res.status(200).send('Welcome to the product page, asm!');
+    //const {username} = req.body || {};
+    if( name && sessionStorage[name] && sessionStorage[name].loggedIn){
+        return res.send(`Welcome to the product page, ${name}`);
     }
     else{
         return res.status(403).send('Access denied. You are not allowed to view this page.');

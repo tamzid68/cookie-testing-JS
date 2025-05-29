2ab7fef2cb4792631a769b8c54ee560cdc0e4665
ASM Tamzid <atkthegreat9999@gmail.com>
2025-05-29 22:43:44 +0600

    appv1 handwriting session login and app lirary session storage

--- app.js
+++ app.js
@@ -1,39 +1,57 @@
 const express = require('express');
 const app = express();
-const cookieParser = require('cookie-parser');
 
-
-app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const sessionStorage = {};
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
-app.post('/login', (req, res) => {
-    const { username } = req.body || {};
-    if (!username) {
-        return res.status(400).send('username is required');
-    }
-    res.cookie('name', username, {
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'signature',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
         maxAge: 900000, // 15 minutes
-        httpOnly: true, // Prevents client-side JavaScript from accessing the cookie 
-    });
-    sessionStorage[username] = {loggedIn: true};
-    res.send('Cookie is set');
+        httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+    }
+})
+);
+
+
+app.post('/login', (req,res)=>{
+    const {username} = req.body || {};
+
+    if(!username) return res.status(400).send('Username required');
+    
+    req.session.username = username;
+    req.session.loggedIn = true;
+    res.send('Session is set');
 });
+
 
 app.get('/product', (req, res)=>{
-    const {name} = req.cookies || {};
-    //const {username} = req.body || {};
-    if( name && sessionStorage[name] && sessionStorage[name].loggedIn){
-        return res.send(`Welcome to the product page, ${name}`);
+    if(req.session.loggedIn){
+        res.send(`Welcome to the product page, ${req.session.username}`);
     }
     else{
-        return res.status(403).send('Access denied. You are not allowed to view this page.');
+        res.status(403).send('Access denied. You are not allowed to view this page.');
     }
 });
 
+app.post('/logout', (req, res)=>{
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(500).send('Could not log out');
+        }
+        res.clearCookie('connect.sid'); // Clear the session cookie
+        res.send('Logged out successfully');
+    });
+})
 
-app.listen(3000, ()=>{
+
+app.listen(3000, () => {
     console.log('Server is running on localhost:3000');
 });
---
+++ appv1.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const sessionStorage = {};
+
+app.post('/login', (req, res) => {
+    const { username } = req.body || {};
+    if (!username) {
+        return res.status(400).send('username is required');
+    }
+    res.cookie('name', username, {
+        maxAge: 900000, // 15 minutes
+        httpOnly: true, // Prevents client-side JavaScript from accessing the cookie 
+    });
+    sessionStorage[username] = {loggedIn: true};
+    res.send('Cookie is set');
+});
+
+app.get('/product', (req, res)=>{
+    const {name} = req.cookies || {};
+    //const {username} = req.body || {};
+    if( name && sessionStorage[name] && sessionStorage[name].loggedIn){
+        return res.send(`Welcome to the product page, ${name}`);
+    }
+    else{
+        return res.status(403).send('Access denied. You are not allowed to view this page.');
+    }
+});
+
+
+app.listen(3000, ()=>{
+    console.log('Server is running on localhost:3000');
+});
